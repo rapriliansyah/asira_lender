b60428deb114cce430037b26e88d5a91ffcaa3ef
Christian <cwinata@ayannah.com>
2019-08-06 18:25:12 +0700

    ignore first path

--- router/router.go
+++ router/router.go
@@ -5,10 +5,16 @@ import (
 	"asira_lender/handlers"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func NewRouter() *echo.Echo {
 	e := echo.New()
+
+	// ignore /api-lender
+	e.Pre(middleware.Rewrite(map[string]string{
+		"/api-lender/*": "/$1",
+	}))
 
 	// e.GET("/test", handlers.Test)
 	e.GET("/clientauth", handlers.ClientLogin)
